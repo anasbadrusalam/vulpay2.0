ac9c5143b1ed6dde089cb15759c817ee1dfb05c4
anasbadrusalam <anasbadrusalam@gmail.com>
2022-06-22 15:04:10 +0700

    WIP

---
+++ app/Nova/InboxRule.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Nova;
+
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+class InboxRule extends Resource
+{
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var string
+     */
+    public static $model = \App\Models\InboxRule::class;
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'id';
+
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search = [
+        'id',
+    ];
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function fields(NovaRequest $request)
+    {
+        return [
+            ID::make()->sortable(),
+
+            Text::make('Sender')
+                ->rules('required'),
+
+            Text::make('Keyword')
+                ->rules('required'),
+
+            Text::make('Regex Number', 'regex_number')
+                ->rules('required'),
+
+            Text::make('Regex Amount', 'regex_amount')
+                ->rules('required'),
+        ];
+    }
+
+    /**
+     * Get the cards available for the request.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function cards(NovaRequest $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function filters(NovaRequest $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function lenses(NovaRequest $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function actions(NovaRequest $request)
+    {
+        return [];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 })->name('login');
+
+
+Route::get('/testing', function () {
+    $keyword = "/(?<=sebesar ).\d+/";
+    $keyword = str_replace("'", "", $keyword);
+
+    // dd($keyword);
+    $input_line = "Disaat Anda memerlukan pulsa, tersedia layanan Pulsa Siaga utk Anda ambil di *911#. Anda menerima Pulsa dari 62818310534111 sebesar 200000.";
+    preg_match('/(\+62|62|0)[8][1-9][0-9]{6,9}/', $input_line, $number);
+    preg_match($keyword, $input_line, $amount);
+    dd($amount);
+    // return view('welcome');
+});
